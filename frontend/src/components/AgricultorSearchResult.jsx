d1ebcb85fb71ccc3a224607d527858488d45417b
elian bourdu <elian.bourdu@gmail.com>
2023-10-27 15:24:50 +0200

    home style

--- frontend/src/components/AgricultorSearchResult.jsx
+++ frontend/src/components/AgricultorSearchResult.jsx
@@ -6,11 +6,11 @@ function AgricultorSearchResult() {
   console.log(searchResult)
   if (searchResult.length !== 0) {
     return (
-      <div>
+      <div className="container">
         <h2>Résultats de recherche :</h2>
         <ul className="searchResponse__container">
           {searchResult.map(user => (
-            <li className="searchResponse__user" key={user.id}>{user.firstName} {user.lastName}</li>
+            <li className="searchResponse__user" key={user.id}>{user.firstName} {user.lastName}  -  situé à : {user.localisation}</li>
           ))}
         </ul>
       </div>
